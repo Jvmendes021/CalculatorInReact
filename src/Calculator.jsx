0af067d5f536c79c8fefa09ddf597251670a47a5
João Vitor Mendes <joao.vitor13mendes@gmail.com>
2023-02-28 14:19:31 -0300

    Primeira etapa dos números e botões concluída.

---
+++ src/Calculator.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Calculator.css';
+import { Container, Hidden } from '@mui/material';
+import { Box } from '@mui/system';
+
+export default function Calculator() {
+  return (
+    <div>
+        <Box m={5} />
+        <Container maxWidth='xs'>
+            <div className='wrapper'>
+                <Box m={12} />
+                <h1 className='result'>00</h1>
+                <button>AC</button>
+                <button>+/-</button>
+                <button>%</button>
+                <button className='orange'>/</button>
+                <button className='gray'>7</button>
+                <button className='gray'>8</button>
+                <button className='gray'>9</button>
+                <button className='orange'>X</button>
+                <button className='gray'>4</button>
+                <button className='gray'>5</button>
+                <button className='gray'>6</button>
+                <button className='orange'>-</button>
+                <button className='gray'>1</button>
+                <button className='gray'>2</button>
+                <button className='gray'>3</button>
+                <button className='orange'>+</button>
+                <button className='gray'>0</button>
+                <button className='gray'>,</button>
+                <button className='gray' style={{visibility: "Hidden"}}>,</button>
+                <button>=</button>
+            </div>
+        </Container>
+    </div>
+  )
+}
